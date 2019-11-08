4e7c0cfdaf0d302b08dd1d11022e23513dff1f99
agonza31 <56164575+agonza31@users.noreply.github.com>
2019-11-08 12:59:46 -0500

    altered 2nd problem solution to take dataset as input
    (as opposed to file name, previously)

--- irisAnalysis2.R
+++ irisAnalysis2.R
@@ -1,30 +1,28 @@
 #solution to problem 2, exercise 7
 
 #function that counts number of observations for a given species
-# inputs: fileName = name of file, specName = species to count
+# inputs: data = name of dataset, specName = species to count
 # observations for
-specObs<-function(fileName,specName){
-  data<-read.csv(fileName)
+specObs<-function(data,specName){
   return(length(which(data$Species==specName)))
 }
 
 #function that returns a dataframe for flowers with Sepal.Width
 #greater than a value specified by the function user
-# inputs: fileName = name of file, sepalWidth = sepal.Width
-sepalWidths<-function(fileName,sepalWidth){
-  data<-read.csv(fileName)
-  data2<-data[data$Sepal.Width>sepalWidth,]
-  return(data2)
+# inputs: data = name of dataset, sepalWidth = sepal.Width
+sepalWidths<-function(data,sepalWidth){
+  return(data[data$Sepal.Width>sepalWidth,])
 }
 
 #function that write the data for a given species to a comma-delimited
 #file with the given species name as the file name
 #column names included at top of each ".csv" file.
-writeData<-function(fileName){
-  data<-read.csv(fileName)
+#inputs: data = name of dataset
+writeData<-function(data){
   for(num in 1:nrow(data)){
     if(num %% 50 == 1) bool <- TRUE
     else if(num > 1) bool <- FALSE
     write.table(data[num,],file=paste(data[num,5],".csv",sep=""),col.names=bool,sep=",",append=TRUE)
   }
+  return()
 }
